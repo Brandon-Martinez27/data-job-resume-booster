f564059bd4b17596bc8f3a038f7cd2a689336b7c
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-06-05 21:31:28 -0500

    script for word counts added into final notebook

--- explore.py
+++ explore.py
@@ -29,3 +29,17 @@ def freq_by_label(da_words, ds_words, de_words, mle_words, all_words):
     all_freq = pd.Series(all_words).value_counts()
 
     return da_freq, ds_freq, de_freq, mle_freq, all_freq
+
+def word_count_label(da_freq, ds_freq, de_freq, mle_freq, all_freq):
+    # combine all into a dataframe to work with
+    word_counts = (pd.concat([all_freq, ds_freq, da_freq, de_freq, mle_freq], axis=1, sort=True)
+                    .set_axis(['all', 'DS', 'DA', 'DE', 'MLE'], axis=1, inplace=False)
+                    .fillna(0)
+                    .apply(lambda s: s.astype(int)))
+    # add the proportion of each label in relation to all other categories
+    word_counts['prop_ds'] = word_counts['DS']/word_counts['all']
+    word_counts['prop_da'] = word_counts['DA']/word_counts['all']
+    word_counts['prop_de'] = word_counts['DE']/word_counts['all']
+    word_counts['prop_mle'] = word_counts['MLE']/word_counts['all']
+
+    return word_counts
